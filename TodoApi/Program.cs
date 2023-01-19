cd04466e838a16f12440b4cf5b94e3bf504f1840
Michael Glaviano <michael.glaviano@gmail.com>
2023-01-19 17:48:31 -0600

    register dependencies lol

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using TodoApi.Infrastructure;
 using TodoApi.Models;
+using TodoApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<TodoService>();
+builder.Services.AddScoped<TodoRepository>();
 
 var app = builder.Build();
 
